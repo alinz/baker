2dba36663cf5450db36c00c2c9a931e4c2df24d1
Ali Najafizadeh <a.najafizadeh@gmail.com>
2019-11-03 23:36:10 -0500

    Add common data

---
+++ data.go
@@ -0,0 +1,24 @@
+package bake
+
+import (
+	"github.com/alinz/bake/pkg/endpoint"
+)
+
+type Config struct {
+	Domain string `json:"domain"`
+	Path   string `json:"path"`
+}
+
+type Container struct {
+	ID       string            `json:"id"`
+	Active   bool              `json:"active"`
+	Addr     endpoint.Addr     `json:"addr"`
+	PingAddr endpoint.HTTPAddr `json:"ping_addr"`
+	Err      error             `json:"error"`
+}
+
+type Service struct {
+	Container *Container `json:"container"`
+	Config    *Config    `json:"config"`
+	Err       error      `json:"error"`
+}
