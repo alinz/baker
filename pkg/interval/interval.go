d2873d707a99fa317dd77609088f995006a01b02
Ali Najafizadeh <a.najafizadeh@gmail.com>
2019-11-04 20:34:14 -0500

    Add interval test

--- pkg/interval/interval.go
+++ pkg/interval/interval.go
@@ -5,10 +5,15 @@ import (
 	"time"
 )
 
+// Ticker is a simple interface which defines how to implement
+// interval
 type Ticker interface {
 	Tick(ctx context.Context) error
 }
 
+// Run calls ticker.Tick method based on duration
+// The given context can be canceled to stop the Run
+// NOTE: this method is blocking
 func Run(ctx context.Context, action Ticker, duration time.Duration) error {
 	ticker := time.NewTicker(duration)
 	defer ticker.Stop()
---
+++ pkg/interval/interval_test.go
@@ -0,0 +1,37 @@
+package interval_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/alinz/bake/pkg/interval"
+)
+
+type DummyTickerFn func(ctx context.Context) error
+
+func (dt DummyTickerFn) Tick(ctx context.Context) error {
+	return dt(ctx)
+}
+
+var _ interval.Ticker = (*DummyTickerFn)(nil)
+
+func TestInterval(t *testing.T) {
+	count := 1
+	dummyTicker := DummyTickerFn(func(cxt context.Context) error {
+		count++
+		return nil
+	})
+
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+
+	err := interval.Run(ctx, dummyTicker, 500*time.Millisecond)
+	if err.Error() != "context deadline exceeded" {
+		t.Fatalf("expected '%s' error but got this '%s'", "context deadline exceeded", err.Error())
+	}
+
+	if count != 5 {
+		t.Fatal("count is not incremented", count)
+	}
+}
